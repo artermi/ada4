c1171dea1dd96cc12ae12404c00877c66c9b90b7
unknown <b03902107@219-35.pclab.csie.ntu.edu.tw>
2015-10-23 12:33:50 +0800

    12

--- 4/4.cpp
+++ 4/4.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <array>
 
+#define lli long long int
 using namespace std;
 
+
+
 int main (){
+	int test_case;
+	lli lower_bound,upper_bound;
+	int digit_table[18][7][18][18] = {0};
+	initialize(digit_table);
+	
+	cin >> test_case;
+	while(test_case --){
+		cin >> lower_bound >> upper_bound;
+		
+		cout << 
+	}
 	return 0;
 }
